bc913a24503698cdf8118cfa87a1fd52e79ad77d
Alena Konovalova <konovalova.alena@gmail.com>
2021-11-22 22:15:34 +0300

    Disable ultimate-sitemap-parser's logging

--- sitemap_urls_auditor/logger/main_logger.py
+++ sitemap_urls_auditor/logger/main_logger.py
@@ -1,5 +1,19 @@
 """This module configures project's main logger."""
 
-from loguru import logger as main_logger
+import logging
 
-main_logger.disable('usp')
+from loguru import Logger, logger
+
+
+def disable_usp_logging(level: str) -> None:
+    logging.getLogger('usp.helpers').setLevel(level)
+    logging.getLogger('usp.fetch_parse').setLevel(level)
+    logging.getLogger('usp.tree').setLevel(level)
+
+
+def get_loguru_logger() -> Logger:
+    return logger
+
+
+disable_usp_logging()
+main_logger = get_loguru_logger()

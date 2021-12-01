5b6521e7135007b098a0652ed9ac25ca73bdddc7
Alena Konovalova <konovalova.alena@gmail.com>
2021-12-01 19:49:52 +0300

    Add docs and type hints

--- sitemap_urls_auditor/logger/main_logger.py
+++ sitemap_urls_auditor/logger/main_logger.py
@@ -1,17 +1,28 @@
 """Module configures project's main logger."""
 
 import logging
+from typing import Union
 
 from loguru import logger
 
 
-def disable_usp_logging(level: str) -> None:
+def disable_usp_logging(level: Union[str, int]) -> None:
+    """Disable logging of ultimate-sitemap-parser (usp) library.
+
+    Args:
+        level: Logging level to be set for usp library loggers.
+    """
     logging.getLogger('usp.helpers').setLevel(level)
     logging.getLogger('usp.fetch_parse').setLevel(level)
     logging.getLogger('usp.tree').setLevel(level)
 
 
 def get_loguru_logger():
+    """Get loguru Logger object.
+
+    Returns:
+        Loguru Logger object.
+    """
     return logger
 
 
